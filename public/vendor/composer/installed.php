bb1f5cb9b7f3c9885b3f945a676a409f704ff14b
Abhishek Kumar Pathak <officialabhishekpathak@gmail.com>
2022-09-17 21:23:13 +0530

    vender updated

--- public/vendor/composer/installed.php
+++ public/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c386f5ec46ef216e1b2d2ca210330b02aa445408',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c386f5ec46ef216e1b2d2ca210330b02aa445408',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
