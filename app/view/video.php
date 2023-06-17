af68b9058c6810cb4cecb4ae9e70eb1d85003f3c
Abhishek-Kumar-Pathak <officialabhishekpathak@gmail.com>
2023-06-17 16:00:36 +0530

    video path var changed

--- app/view/video.php
+++ app/view/video.php
@@ -32,7 +32,7 @@
 <body>
 
     <video id="video-js" class="video-js vjs-default-skin vjs-big-play-centered vjs-fill" controlslist="nodownload" preload="auto" controls>
-        <source src="<?php echo $videoPath ?>" type="application/x-mpegURL">
+        <source src="<?php echo $path; ?>" type="application/x-mpegURL">
         <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
     </video>
 
